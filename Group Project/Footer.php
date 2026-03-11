f4dd985530d24ebb1c77aa490f7018d642d508df
L-Riddle06 <142291315+L-Riddle06@users.noreply.github.com>
2026-03-11 15:38:53 +0000

    Footer.php

--- Group Project/Footer.php
+++ Group Project/Footer.php
@@ -1,6 +1,7 @@
 <footer class = "custom-footer p-3 mt-auto text-center w-100">
-    <p>&copy; 2025 S&M Hotels. All rights reserved.</p>
+    <p>&copy; Laurence Riddle, Anthony Richardson, Jamie Overend, Likhith Kumar. All Rights Reserved and all that jazz.</p>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
+
