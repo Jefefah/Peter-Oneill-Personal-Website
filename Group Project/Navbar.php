5e3a89d79595ec68cb203677cb1ea00012c1c4e2
Jefefah <antj.richie@gmail.com>
2026-03-11 15:53:16 +0000

    dsfdsf

--- Group Project/Navbar.php
+++ Group Project/Navbar.php
@@ -18,7 +18,7 @@ session_start();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="styles.css?v=<?= time();?>">
+    <link rel="stylesheet" href="styles?v=<?= time();?>">
     <title>S&M Hotels</title>
 </head>
 
