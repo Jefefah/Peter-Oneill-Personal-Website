eb12443d75de2d0a1b2fad1f2c7a4c13fb5dfff5
Jefefah <antj.richie@gmail.com>
2026-03-11 15:16:55 +0000

    Update Footer.php

---
+++ Group Project/Footer.php
@@ -0,0 +1,6 @@
+<footer class = "custom-footer p-3 mt-auto text-center w-100">
+    <p>&copy; 2025 S&M Hotels. All rights reserved.</p>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
