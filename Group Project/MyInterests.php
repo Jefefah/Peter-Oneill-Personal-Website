9eb19988298b8738bcc8c24ef1cba887c05af170
L-Riddle06 <142291315+L-Riddle06@users.noreply.github.com>
2026-03-18 14:59:38 +0000

    why wont it update

---
+++ Group Project/MyInterests.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+    <h1>My Interests</h1>
+
+    <h2>Interest 1</h2>
+    <p>First interest is ______, got into it for whatever reason and do it however often</p>
+    <h2>Interest 2</h2>
+    <p>Second interest is ______, got into it for whatever reason and do it however often</p>
+
+
+
+</body>
+<!-- edit button orange right, cancel button red left, confirm green right-->
+</html>
