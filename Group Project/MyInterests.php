447da76c909c2ec80fddbe891eeb1591baae046c
L-Riddle06 <142291315+L-Riddle06@users.noreply.github.com>
2026-03-18 15:35:51 +0000

    l

--- Group Project/MyInterests.php
+++ Group Project/MyInterests.php
@@ -1,14 +1,20 @@
+<?php
+include "Navbar.php"; 
+?>
+
 <html>
 <body>
-    <h1>My Interests</h1>
+    <h1 class="mb-3">My Interests</h1>
 
-    <h2>Interest 1</h2>
-    <p>First interest is ______, got into it for whatever reason and do it however often</p>
-    <h2>Interest 2</h2>
-    <p>Second interest is ______, got into it for whatever reason and do it however often</p>
+    <h2 class="mb-3">Interest 1</h2>
+    <p class="text-wrap">First interest is ______, got into it for whatever reason and do it however often</p>
+    <h2 class="mb-3">Interest 2</h2>
+    <p class="text-wrap">Second interest is ______, got into it for whatever reason and do it however often</p>
 
 
 
 </body>
 <!-- edit button orange right, cancel button red left, confirm green right-->
 </html>
+
+<?php include_once "Footer.php"; ?>
